8c7ea3f180f8657cebf726d1a0abfe54b76a7ba2
unknown <henrique.dcti@gmail.com>
2020-11-26 16:05:48 -0300

    Add unique and null values

--- db/migrate/20201126143046_create_users.rb
+++ db/migrate/20201126143046_create_users.rb
@@ -3,14 +3,14 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users, id: :uuid do |t|
       t.string :name
       t.date :birthday_date
-      t.string :email
+      t.string :email, :unique => true
       t.string :password
-      t.string :description
-      t.string :avatar
-      t.string :phone_number
-      t.string :telegram
-      t.string :facebook
-      t.string :whatsapp
+      t.string :description, :null => true
+      t.string :avatar, :null => true
+      t.string :phone_number, :null => true, :limit => 13 #By default SQL String limit 255 character 
+      t.string :telegram, :null => true
+      t.string :facebook, :null => true
+      t.string :whatsapp, :null => true
 
       t.timestamps
     end

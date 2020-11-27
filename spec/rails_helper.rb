f3fda4ac86527310016a0a9d68d41e95f58f37d4
unknown <henrique.dcti@gmail.com>
2020-11-27 16:23:09 -0300

    Add JSON helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,6 +16,13 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f}
+
+RSpec.configuration do |config|
+  config.include RequestSpecHelper, type: :request
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"

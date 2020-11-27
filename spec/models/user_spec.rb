9c62d9472299d706e10d3bbdeb86106dbfb8d33d
unknown <henrique.dcti@gmail.com>
2020-11-27 13:32:51 -0300

    Add user rspec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:user_tokens).dependent(:destroy)}
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:birthday_date) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
 end

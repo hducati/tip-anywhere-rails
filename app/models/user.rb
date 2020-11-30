8e3b98086826f17a25acc0e63ec50495f566615c
unknown <henrique.dcti@gmail.com>
2020-11-30 14:21:51 -0300

    Add secure_password method and bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # encrypt password
+  has_secure_password
+
   has_many :user_tokens, dependent: :destroy
 
   validates_presence_of :name, :birthday_date, :email, :password

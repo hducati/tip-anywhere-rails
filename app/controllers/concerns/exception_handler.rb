f4ee9ee2ed25675918cedfa5f8909078b04cb1ca
unknown <henrique.dcti@gmail.com>
2020-11-30 14:38:23 -0300

    Add token exception handler

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,13 +1,28 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+
   included do
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_request
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :unprocessable_entity_request
+    rescue_from ExceptionHandler::InvalidToken, with: :unprocessable_entity_request
+
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
     end
+  end
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
-    end
+  private
+
+  def unprocessable_entity_request(error)
+    json_response({ message: e.message }, :unprocessable_entity)
+  end
+
+  def unauthorized_request(error)
+    json(response({ message: e.message }, :unauthorized_request))
   end
 end

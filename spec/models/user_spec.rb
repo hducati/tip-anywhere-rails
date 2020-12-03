3989c0a8a8330f8bfb1478e82bbb7f9430c0a8b2
unknown <henrique.dcti@gmail.com>
2020-12-03 18:41:25 -0300

    Add uniqueness validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,5 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:birthday_date) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password_digest) }
+  it { should validate_uniqueness_of(:email)}
 end

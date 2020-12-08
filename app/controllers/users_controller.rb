5cb3074e940fa799bdbc94f5cf4f9899a0d7dd2b
Henrique Ducati <henrique.dcti@gmail.com>
2020-12-08 13:56:32 -0300

    Rubocop spaces fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     user = User.create!(user_params)
     auth_token = AuthenticateUserService.execute(user.email, user.password)
-    response = { message: Message.account_created, auth_token: auth_token}
+    response = { message: Message.account_created, auth_token: auth_token }
     json_response(response, :created)
   end
 

f4f962c380c2f5b247db650e8fd351fa27fc876b
Henrique Ducati <henrique.dcti@gmail.com>
2020-12-10 10:05:18 -0300

    Add validate_numericality to odd and unit

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -2,4 +2,6 @@ class Tip < ApplicationRecord
   belongs_to :user
 
   validates :odd, :tip, :game, :unit, :user_id, presence: true
+
+  validates :odd, :unit, numericality: { greater_than_or_equal_to: 0.01 }
 end
--- spec/models/tip_spec.rb
+++ spec/models/tip_spec.rb
@@ -6,4 +6,6 @@ RSpec.describe Tip, type: :model do
   it { is_expected.to validate_presence_of(:tip) }
   it { is_expected.to validate_presence_of(:game) }
   it { is_expected.to validate_presence_of(:unit) }
+  it { is_expected.to validate_numericality_of(:odd).is_greater_than_or_equal_to(0.01) }
+  it { is_expected.to validate_numericality_of(:unit).is_greater_than_or_equal_to(0.01) }
 end

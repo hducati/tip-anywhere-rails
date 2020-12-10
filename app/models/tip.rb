d128cd40f4fec8340c831322094e16f28b8975ae
Henrique Ducati <henrique.dcti@gmail.com>
2020-12-10 10:57:57 -0300

    Add status spec to Tip's model

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -4,4 +4,5 @@ class Tip < ApplicationRecord
   validates :odd, :tip, :game, :unit, :user_id, presence: true
 
   validates :odd, :unit, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :status, allow_blank: true, inclusion: { in: %w[Half Red Green Void] }
 end
--- spec/models/tip_spec.rb
+++ spec/models/tip_spec.rb
@@ -8,4 +8,5 @@ RSpec.describe Tip, type: :model do
   it { is_expected.to validate_presence_of(:unit) }
   it { is_expected.to validate_numericality_of(:odd).is_greater_than_or_equal_to(0.01) }
   it { is_expected.to validate_numericality_of(:unit).is_greater_than_or_equal_to(0.01) }
+  it { is_expected.to validate_inclusion_of(:status).in_array(%w[Half Red Green Void]) }
 end
